80710247c901580331cb707e338f9166f5f9cd19
deepnavy <petrov.evpanet@gmail.com>
2018-01-04 18:43:34 +0200

    Fixes based on comments
    - left thread_section_style macros only in forum_base.html
    - load all resourses via fantastic
    - deleted all commented chunks of code

--- ckanext/forum/fanstatic/javascript/forms_validate.js
+++ ckanext/forum/fanstatic/javascript/forms_validate.js
@@ -1,25 +1,6 @@
 autosize($('textarea'));
 
-// $.validator.setDefaults({ ignore: ":hidden:not(select)" });
-
-// console.log($.validator)
-
 $.validator.setDefaults({ ignore: ":hidden:not(select)" });
-
-// validation of chosen on change
-// if ($("select.custom-select").length > 0) {
-//     $("select.custom-select").each(function() {
-//         if ($(this).attr('required') !== undefined) {
-//             $(this).change(function() {
-//                 $(this).valid();
-//             });
-//         }
-//     });
-// }
-
-// $("select.custom-select").change(function() {
-//     $(this).valid();
-// });
 
 $('#thread-form').validate({
     errorPlacement: function (error, element) {
@@ -34,20 +15,6 @@ $('#thread-form').validate({
         }
     },
     
-    
 });
-
-
-    // console.log($.validator) 
-    // $.validator.setDefaults({ ignore: ":hidden:not(.chosen-select)" })
-    // $.fn.validator.Constructor.INPUT_SELECTOR = ':input:not([type="hidden"], [type="submit"], [type="reset"], button)'
-    // $("#thread-form").validate();
-    // // $("#thread-form").validator('update');
-
-    // $.validator.setDefaults({ ignore: ":hidden:not(.chosen-select)" });
-    // $("#thread-form").validate({
-    //     rules: {chosen:"required"},
-    //     message: {chosen:"Select a Country"}
-    // });
 
 
