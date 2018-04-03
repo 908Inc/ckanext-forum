645f8cdd3039e63c110fd9b4d01c52f8978fcc0e
DaleDMT <daledmt@gmail.com>
2018-04-03 22:54:33 +0300

    added btn prop disabled if form submitted

--- ckanext/forum/fanstatic/javascript/forum.js
+++ ckanext/forum/fanstatic/javascript/forum.js
@@ -1,3 +1,6 @@
 $('div.thread-body').click(function () {
    window.location.href = $(this).data('thread-url');
 });
+$('form#forum-post').submit(function (e) {
+    $('#post-submit').prop('disabled', true);
+});
