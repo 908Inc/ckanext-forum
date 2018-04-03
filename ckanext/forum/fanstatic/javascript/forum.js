6ec02aabdb8ee7a812fcc6ccf4ce3757e4352c43
DaleDMT <daledmt@gmail.com>
2018-04-03 23:06:52 +0300

    added btn prop disabled if form submitted (create thread, create board)

--- ckanext/forum/fanstatic/javascript/forum.js
+++ ckanext/forum/fanstatic/javascript/forum.js
@@ -1,6 +1,11 @@
 $('div.thread-body').click(function () {
    window.location.href = $(this).data('thread-url');
 });
-$('form#forum-post').submit(function (e) {
+
+$('form#forum-post').submit(function () {
     $('#post-submit').prop('disabled', true);
+});
+
+$('form#thread-form').submit(function () {
+    $('#thread-submit').prop('disabled', true);
 });
